ab333ddf7debbf16d07299445f3814e5967a52f9
Ryan L Nelson-Flack <rn01094@srcs.loc>
2017-01-24 13:44:24 -0800

    test

--- rochambeau.js
+++ rochambeau.js
@@ -1,7 +1,6 @@
 // This is the varaible that stores the score.
 // score[0] = wins, score[1] = ties, score[2] = losses
 var score = [0, 0, 0];
-
 // The variables store the current player's and computer's choices
 // 0 = Rock, 1 = Paper, 2 = Scissors
 var playerChoice;
@@ -13,45 +12,61 @@ function playGame() {
         // We have a tie!
         console.log("tie");
         return 0;
-    } else if (playerChoice == 0 && computerChoice == 2 || computerChoice == 3) {
-        // Rock beats scissors and lizard - a win!
-        console.log("win");
-        return 1;
-    } else if (playerChoice == 1 && computerChoice == 0 || computerChoice == 4) {
-        // Paper beats rock and spock - a win!
-        console.log("win");
-        return 1;
-    } else if (playerChoice == 2 && computerChoice == 1 || computerChoice == 3) {
-        // Scissors beats paper and lizard - a win!
-        console.log("win");
-         return 1;
-    } else if (playChoice == 3 && computerChoice == 1 || computerChoice == 4){
+    }
+    else if (playerChoice == 0) {
+        if (computerChoice == 2 || computerChoice == 3) {
+            // Rock beats scissors and lizard - a win!
+            console.log("win");
+            return 1;
+        }
+    }
+    else if (playerChoice == 1) {
+        if (computerChoice == 0 || computerChoice == 4) {
+            // Paper beats rock and spock - a win!
+            console.log("win");
+            return 1;
+        }
+    }
+    else if (playerChoice == 2) {
+        if (computerChoice == 1 || computerChoice == 3) {
+            // Scissors beats paper and lizard - a win!
+            console.log("win");
+            return 1;
+        }
+    }
+    else if (playChoice == 3) {
+        if (computerChoice == 1 || computerChoice == 4) {
             //Lizard beats paper and spock - a win!
             console.log("win");
-         return 1;
-    } else if (playerChoice == 4 && computerChoice == 0 || computerChoice == 2){
+            return 1;
+        }
+    }
+    else if (playerChoice == 4) {
+        if (computerChoice == 0 || computerChoice == 2) {
             //Spock beats rock and scissors - a win!
             console.log("win");
-         return 1;
-        } else {
+            return 1;
+        }
+    }
+    else {
         // All other combinations are losses
         console.log("lose");
         return -1;
     }
 }
 
-function displayScoreBoard(winsId, lossesId, tiesId){
+function displayScoreBoard(winsId, lossesId, tiesId) {
     document.getElementById(winsId).innerHTML = score[0];
     document.getElementById(lossesId).innerHTML = score[2];
     document.getElementById(tiesId).innerHTML = score[1];
 }
 
-function updateScore(val){
+function updateScore(val) {
     ++score[val];
     console.log("The score is now " + score);
 }
 
-function displayGameResult(resultId){
+function displayGameResult(resultId) {
     // Define an array of text labels for the choices 0, 1, 2;
     var choices = ["Rock", "Paper", "Scissors", "Lizard", "Spock"];
     // Now play the game and store the result
@@ -64,12 +79,14 @@ function displayGameResult(resultId){
         updateScore(0);
         document.getElementById(resultId).innerHTML = message + "YOU WIN!";
         document.getElementById(resultId).className = "alert alert-success";
-    } else if (result == -1) {
+    }
+    else if (result == -1) {
         updateScore(2);
         // Display that it was a loss
         document.getElementById(resultId).innerHTML = message + "YOU LOOSE! ";
         document.getElementById(resultId).className = "alert alert-danger";
-    } else {
+    }
+    else {
         // Display that it was a tie
         updateScore(1);
         document.getElementById(resultId).innerHTML = message + "A tie. ";
@@ -85,6 +102,6 @@ function storePlayerChoice(choice) {
 
 function storeComputerChoice() {
     // Generate computer's random choice
-    computerChoice = Math.floor(Math.random()*5);
+    computerChoice = Math.floor(Math.random() * 5);
     console.log("Computer choice = " + computerChoice);
 }
